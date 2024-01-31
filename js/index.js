299b8da0b3888248b4fbc744f9b17d22fdff15f6
NaN-jp <51327367+NaN-jp@users.noreply.github.com>
2024-01-31 12:33:47 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
-function test() {
-    navigator.geolocation.getCurrentPosition(jsToGas);
+var textArea;
+var nowDate;
+var longitude;
+var latitude;
+
+var optionObj = {
+	"enableHighAccuracy": ture ,
+	"timeout": 8000 ,
+	"maximumAge": 5000 ,
+} ;
+
+function jsToGas(){
+    navigator.geolocation.getCurrentPosition(successFunc, errorFunc, optionObj);
 }
 
-function jsToGas(position){
-    var comment = document.getElementById("commentArea").ariaValueText;
-    var date = new Date(position.timestamp);
-    var string = "commnet" + "\n";
-    string += comment + "\n";
-    string += "timestamp" + "\n";
-    string += date;
-    alert(string);
+function successFunc(position){
+    textArea = document.getElementById("commentArea").ariaValueText;
+    nowDate = new Date(position.timestamp);
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    
+    alert(textArea + "\n" + nowDate + "\n" + longitude + "\n" + latitude);
+}
+
+function errorFunc(error){
+    var errorMessage = {
+		0: "原因不明のエラーが発生しました…。" ,
+		1: "位置情報の取得が許可されませんでした…。" ,
+		2: "電波状況などで位置情報が取得できませんでした…。" ,
+		3: "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。" ,
+	};
+    alert(errorMessage[error.code] ) ;
 }
