38caa70fca712ea9c75cdbf01df9c2fa959837ab
NaN-jp <51327367+NaN-jp@users.noreply.github.com>
2024-02-09 10:12:59 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,44 @@
-var textArea;
-var nowDate;
-var longitude;
-var latitude;
+let team;
+let textArea;
+let longitude;
+let latitude;
 
-var optionObj = {
+const { callTs } = require('../ts/dataInsert');
+
+const optionObj = {
 	"enableHighAccuracy": true ,
 	"timeout": 8000 ,
 	"maximumAge": 0 ,
-} ;
+};
 
-function exportCsv(){
+const errorMessage = {
+    0: "原因不明のエラーが発生しました…。" ,
+    1: "位置情報の取得が許可されませんでした…。" ,
+    2: "電波状況などで位置情報が取得できませんでした…。" ,
+    3: "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。" ,
+};
+
+function saveMysql(){
     navigator.geolocation.getCurrentPosition(successFunc, errorFunc, optionObj);
 }
 
 function successFunc(position){
-    var csv = new XMLHttpRequest().open("GET", "data.csv", false);
+    //チーム
+    team = document.getElementById("team").value;
+    //コメント
     textArea = document.getElementById("commentArea").value;
+    //緯度
     latitude = position.coords.latitude;
+    //経度
     longitude = position.coords.longitude;
-    
-    var csvStr = longitude + "," + latitude + "," + textArea + "\n";
-    
+    //TSを呼び出す
+    if(!!team || !!textArea){
+        callTs(team, textArea, latitude, longitude);
+    }else{
+        return;
+    }
 }
 
 function errorFunc(error){
-    var errorMessage = {
-		0: "原因不明のエラーが発生しました…。" ,
-		1: "位置情報の取得が許可されませんでした…。" ,
-		2: "電波状況などで位置情報が取得できませんでした…。" ,
-		3: "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。" ,
-	};
     alert(errorMessage[error.code] ) ;
 }
-
